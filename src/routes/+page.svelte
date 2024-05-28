d3fa2e1e38c7a78e6be9ff42f59dec190108ddbd
mymiracle0118 <noahsflood908@gmail.com>
2024-05-28 18:42:40 +0900

    Add template animation card.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,14 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<script>
+	import Card1 from '$lib/components/card/Card1.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+<section>
+	<Card1 />
+</section>
 
 <style lang="postcss">
 	:global(html) {
